864c7dd1c7284f98de889172e825cd0b55bd7b75
Артем <parshyn4@gmail.com>
2023-10-04 15:42:20 +0300

    create-api-v2 apiUser(choice)-done

--- dz_django_new/api/views.py
+++ dz_django_new/api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets
+from rest_framework import viewsets
 
-# Create your views here.
+from api.serializers import UserSerializer, GoodSerializer, StorageSerializer
+from api.models import ApiUser, Good, Storage
+
+
+class UserModelViewSet(viewsets.ModelViewSet):
+    queryset = ApiUser.objects.all()
+    http_method_names = ['post', "path", "get", 'delete']
+    serializer_class = UserSerializer
+
+
+class GoodViewSet(viewsets.ModelViewSet):
+    queryset = Good.objects.all()
+    serializer_class = GoodSerializer
+
+
+class StorageViewSet(viewsets.ModelViewSet):
+    queryset = Storage.objects.all()
+    serializer_class = StorageSerializer
+"""
+class StorageSendToView(viewsets.ModelViewSet):
+    def get_queryset(self, request):
+        pk = self.kwargs.get('pk')
+        try:
+            storage = Storage.objects.get(pk=pk)
+        except Storage.DoesNotExist:
+            return HttpResponse(status=404)
+        if request.method == 'POST':
+            return storage
+        # Логика, использующая значение pk
+        # Например, возвращение фильтрованного queryset по значению pk
+    # return JsonResponse(serializer.errors, status=400)
+
+"""
